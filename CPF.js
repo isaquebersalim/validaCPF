04ac7816daa06dd5f57d4209930402f909d28a29
Isaque Bersalim <isaquebersalim@gmail.com>
2025-05-13 21:43:23 -0300

    Resultado do resto igual a 10 ou 11, resto vira 0.

--- CPF.js
+++ CPF.js
@@ -29,3 +29,9 @@ soma = 0;
 for(let i=1; i <= 10; i++){
     soma += parseInt(cpf.substring(i-1, i)) * (12 - i);
 }
+
+resto = (soma * 10) % 11;
+
+if(resto ===10 || resto === 11){
+    resto = 0;
+}
